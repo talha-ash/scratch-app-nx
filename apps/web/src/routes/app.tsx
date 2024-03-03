7797ddad4c94b8f37ea74e49a34b698eb7c4375a
talha-ash <ta.xit@yandex.com>
2024-03-03 15:38:15 +0500

    set tailwind yheam and fonts

--- apps/web/src/routes/app.tsx
+++ apps/web/src/routes/app.tsx
@@ -10,7 +10,18 @@ export function App() {
   }
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-h1">Heading 1</h1>
+      <h1 className="text-h2">Heading 2</h1>
+      <h1 className="text-h3">Heading 3</h1>
+      <h1 className="text-h4">Heading 4</h1>
+      <h1 className="text-h5">Heading 5</h1>
+      <h1 className="text-cardItem">Card Item</h1>
+      <h1 className="text-lead">Lead</h1>
+      <h1 className="text-body">Body</h1>
+      <h1 className="text-button text-primary">Button</h1>
+      <h1 className="text-textGray">Text Gray</h1>
+      <h1 className="text-textSubtle">Text Subtle</h1>
+      <h1 className="text-caption">Caption</h1>
     </div>
   );
 }
--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -2,7 +2,129 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#30BE76',
+        primaryLight: '#D6F2E4',
+        systemRed: '#F84971',
+        systemYellow: '#F8B449',
+        textBlack: '#030F09',
+        textDarkGray: '#767676',
+        textGray: '#CCCCCC',
+        textLightGray: '#E6E6E6',
+        textLightestGray: '#F7F8FA',
+        overlay: 'rgba(40, 41, 40, 0.5)',
+        shadow: 'rgba(13, 51, 32, 0.1)',
+      },
+      backgroundColor: {
+        black: '#030F09',
+        darkGray: '#767676',
+        gray: '#CCCCCC',
+        lightGray: '#E6E6E6',
+        lightestGray: '#F7F8FA',
+      },
+      fontSize: {
+        h1: [
+          '2.5rem',
+          {
+            lineHeight: 'normal',
+            letterSpacing: '-0.031rem',
+            fontWeight: 600,
+          },
+        ],
+        h2: [
+          '2rem',
+          {
+            lineHeight: '2rem',
+            letterSpacing: '0rem',
+            fontWeight: 700,
+          },
+        ],
+        h3: [
+          '1.5rem',
+          {
+            lineHeight: '1.5rem',
+            letterSpacing: '0rem',
+            fontWeight: 700,
+          },
+        ],
+        h4: [
+          '1.25rem',
+          {
+            lineHeight: 'normal',
+            letterSpacing: '0rem',
+            fontWeight: 700,
+          },
+        ],
+        h5: [
+          '1rem',
+          {
+            lineHeight: 'normal',
+            letterSpacing: '0rem',
+            fontWeight: 700,
+          },
+        ],
+        cardItem: [
+          '1.125rem',
+          {
+            lineHeight: '2rem',
+            letterSpacing: '0rem',
+            fontWeight: 600,
+          },
+        ],
+        lead: [
+          '1rem',
+          {
+            lineHeight: '1.375rem',
+            letterSpacing: '0rem',
+            fontWeight: 400,
+          },
+        ],
+        body: [
+          '0.875rem',
+          {
+            lineHeight: '1.375rem',
+            letterSpacing: '0rem',
+            fontWeight: 400,
+          },
+        ],
+        button: [
+          '1rem',
+          {
+            lineHeight: 'normal',
+            letterSpacing: '0.02rem',
+            fontWeight: 700,
+          },
+        ],
+        textGray: [
+          '1rem',
+          {
+            lineHeight: '1.375rem',
+            letterSpacing: '0rem',
+            fontWeight: 400,
+          },
+        ],
+        textSubtitle: [
+          '1rem',
+          {
+            lineHeight: '1.375rem',
+            letterSpacing: '0rem',
+            fontWeight: 400,
+          },
+        ],
+        caption: [
+          '0.75rem',
+          {
+            lineHeight: 'normal',
+            letterSpacing: '0.025rem',
+            fontWeight: 400,
+          },
+        ],
+      },
+    },
+    fontFamily: {
+      sans: ['Nunito Sans'],
+    },
   },
   plugins: [],
 };
